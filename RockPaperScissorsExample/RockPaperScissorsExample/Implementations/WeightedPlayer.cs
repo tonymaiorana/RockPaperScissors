5d4cc95972f4ba2ed8c7164a7cde73b81b3506ac
tmaiorana23 <tmaiorana23@gmail.com>
2016-03-08 13:35:50 -0500

    RPS-1 added WeightedPlayer class, talked about how to weight it Joe will do that

---
+++ RockPaperScissorsExample/RockPaperScissorsExample/Implementations/WeightedPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RockPaperScissorsExample.Enums;
+
+namespace RockPaperScissorsExample.Implementations
+{
+    public class WeightedPlayer : Player
+    {
+        private static Random _randomGenerator = new Random();
+
+        public WeightedPlayer(string Name) : base(Name)
+        {
+        }
+
+        public override Choice GetChoice()
+        {
+            int i = _randomGenerator.Next(1, 11);
+
+            return (Choice) i;
+        }
+    }
+}
+
