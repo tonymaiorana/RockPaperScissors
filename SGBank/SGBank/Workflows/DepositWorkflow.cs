8f1d5f896c558f58a35381ae5711445c95dff1ad
Thomas Giffels <tegiffels@gmail.com>
2016-03-11 13:21:16 -0500

    RPS-2 added deposit

--- SGBank/SGBank/Workflows/DepositWorkflow.cs
+++ SGBank/SGBank/Workflows/DepositWorkflow.cs
@@ -16,16 +16,31 @@ namespace SGBank.Workflows
         {
             _currentAccount = account;
         }
-        public void MakeDeposit()
+        public Account MakeDeposit()
         {
             decimal amount = PromptUserForDeposit();
             var ops = new AccountOperations();
             ops.MakeDeposit(_currentAccount, amount);
+            return _currentAccount;
         }
 
         public decimal PromptUserForDeposit()
         {
-            
+            do
+            {
+                Console.Write("Enter the amount of your deposit: ");
+                string input = Console.ReadLine();
+
+                decimal depositAmount;
+                if (decimal.TryParse(input, out depositAmount))
+                {
+                    return depositAmount;
+                }
+
+                Console.WriteLine("That is not a valid deposit amount...");
+                Console.WriteLine("Press enter to continue...");
+                Console.ReadLine();
+            } while (true);
         }
     }
 }
